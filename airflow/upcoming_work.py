403a5545f611294f37a25228da8571434893420d
Beyond-Endoscopy <57195937+Beyond-Endoscopy@users.noreply.github.com>
2020-06-28 10:00:19 -0400

    Update upcoming_work.py

--- airflow/upcoming_work.py
+++ airflow/upcoming_work.py
@@ -3,14 +3,9 @@ from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.bash_operator import BashOperator
 
-bashcommand1 = "python3 ~/Artluxe_version1/upcoming_scraper/new_auctions_collector.py"
+bashcommand1 = "python3 ~/Artluxe_version1/new_auctions_collector.py"
 
-bashcommand2 = "python3 ~/Artluxe_version1/upcoming_scraper/upcoming_scraper_job.py"
-
-bashcommand3 = "python3 ~/Artluxe_version1/upcoming_scraper/to_database.py"
-
-bashcommand4 = "python3 ~/Artluxe_version1/upcoming_scraper/monitor_to_db.py"
-
+bashcommand2 = "python3 ~/Artluxe_version1/upcoming_online.py"
 
 with DAG(dag_id = 'upcoming_work', scheduler_interval = @weekly,
      start_date = datetime(2020, 1, 1), catchup=False) as dag:
@@ -21,12 +16,6 @@ with DAG(dag_id = 'upcoming_work', scheduler_interval = @weekly,
 
     artworks = BashOperator(task_id='artworks', bash_command=bashcommand2)
 
-    insert_data = BashOperator(task_id='insert_data', bash_command=bashcommand3)
-
-    running_info = BashOperator(task_id='running_info', bash_command=bashcommand4)
-
     dummy_task >> collect_auctions >> artworks
-    artworks >> insert_data
-    artworks >> running_info
 
 
