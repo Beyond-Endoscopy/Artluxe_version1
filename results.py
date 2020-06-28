d93b01423f6ed679b872bcfd062271dabaf4f0ca
Beyond-Endoscopy <57195937+Beyond-Endoscopy@users.noreply.github.com>
2020-06-28 09:55:52 -0400

    Update results.py

--- results.py
+++ results.py
@@ -1,5 +1,7 @@
-from past_artworks_getter import *
-from update_db import *
+from artworks.past_artworks_getter import *
+from to_database.update_db import *
+
+#The same as the comment in 'upcoming_online.py', instead using 'past_auctions.txt' which stores the past auction links.
 
 r = get_all_artworks_all_auctions(browser, 'past_auctions.txt', 1)
 
