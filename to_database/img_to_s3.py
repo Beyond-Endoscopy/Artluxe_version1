374ef8f0a1f8e80a2ee8d29c74aa5955789df84d
Beyond-Endoscopy <57195937+Beyond-Endoscopy@users.noreply.github.com>
2020-06-28 20:16:14 -0400

    Update img_to_s3.py

--- to_database/img_to_s3.py
+++ to_database/img_to_s3.py
@@ -3,14 +3,13 @@ import shutil
 import boto3
 
 def download_img(img_url, temp_img_location):
-#temp_img_location could be '/home/ubuntu/img.jpg'
+    
+#temp_img_location could be '~/img.jpg'
 
     image = requests.get(img_url, stream=True)
     with open(temp_img_location, 'wb') as output:
         shutil.copyfileobj(image.raw, output)
     print("downloading images...")
-
-    # os.rename('/Users/haoli/Downloads/img.jpg', '/Users/haoli/Downloads/{}.jpg'.format(name))
 
 def upload_to_s3(file_name, bucket_name, folder, object_name):
     s3_client = boto3.client('s3')
